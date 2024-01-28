6f0aad9e5501b0b60653a102df3540b416aa5eda
Mateusz Minder <mateusz.minder99@gmail.com>
2024-01-28 12:37:48 +0100

    illustrative message for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="poppins h-screen w-screen flex flex-col gap-3 items-center justify-center">
       <span>Welcome to Taskingo!</span>
+      <span>In order to access the page, you must be logged in.</span>
       <Link href="/login" className="w-[320px] p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none text-center">To login page</Link>
     </main>
   );
