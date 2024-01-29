7918295cfa46ce0232ec9241f1344d07197a85bd
Mateusz Minder <mateusz.minder99@gmail.com>
2024-01-29 13:44:29 +0100

    boards page setup

---
+++ src/app/boards/page.tsx
@@ -0,0 +1,10 @@
+
+const BoardsPage = () => {
+  return (
+    <main className="poppins w-screen flex flex-col flex-grow gap-10 items-center justify-center p-10 text-white text-center">
+      <span>Boards</span>
+    </main>
+  )
+}
+
+export default BoardsPage
