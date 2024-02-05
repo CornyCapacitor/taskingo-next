aacd0b6dab8bd26b88082c8fc442c18e17ad45f9
Mateusz Minder <mateusz.minder99@gmail.com>
2024-02-05 13:41:46 +0100

    task styling + add new task improved

--- src/app/boards/[id]/page.tsx
+++ src/app/boards/[id]/page.tsx
@@ -13,6 +13,26 @@ const BoardPage = () => {
   const [boards, setBoards] = useState<Board[]>([])
   const [board, setBoard] = useState<Board>()
 
+  // const getInputClass = () => {
+  //   Swal.fire({
+  //     title: 'Dummy input',
+  //     input: 'text',
+  //     showCancelButton: false,
+  //     confirmButtonText: 'Close',
+  //   }).then(() => {
+  //     const inputField = document.querySelector('.swal2-input')
+  //     if (inputField) {
+  //       const inputClass = inputField.getAttribute('class')
+  //       const computedStyles = window.getComputedStyle(inputField)
+  //       console.log('Full class of the input field', computedStyles)
+  //     } else {
+  //       console.error('Input field not found')
+  //     }
+  //   })
+  // }
+
+  // getInputClass();
+
   useEffect(() => {
     if (user && !Object.keys(user).length) return
 
@@ -108,9 +128,18 @@ const BoardPage = () => {
     Swal.fire({
       color: "#fff",
       background: "#111827",
-      title: 'Enter new task name',
-      input: "text",
-      inputPlaceholder: "New task",
+      title: 'Create new task',
+      html: `
+        <input type="text" id="taskName" placeholder="New task name" class="swal2-input custom_swal_input"/>
+        <input type="text" id="taskDescription" placeholder="Short description (optional)" class="swal2-input custom_swal_input"/>
+        <select id="taskSelect" class="custom_swal_select">
+          <option value="" class="custom_swal_option" disabled selected>Choose task label</option>
+          <option value="" class="custom_swal_option">Undefined &xcirc;</option>
+          <option value="unfinished" class="custom_swal_option">Unfinished &#x1F534;</option>
+          <option value="inProgress" class="custom_swal_option">In progress &#x1F7E1;</option>
+          <option value="finished" class="custom_swal_option">Finished &#x1F7E2;</option>
+        </select>
+      `,
       showConfirmButton: true,
       confirmButtonText: "Create",
       confirmButtonColor: "#2563eb",
@@ -118,8 +147,21 @@ const BoardPage = () => {
       cancelButtonText: "Cancel",
     }).then((result) => {
       if (result.isConfirmed) {
-        const taskName = result.value
-        createNewTask({ listId, boardId, taskName })
+        const taskInput = document.getElementById('taskName') as HTMLInputElement
+        const descriptionInput = document.getElementById('taskDescription') as HTMLInputElement
+        const taskSelect = document.getElementById('taskSelect') as HTMLInputElement
+
+        if (taskInput && descriptionInput && taskSelect) {
+          const taskName = taskInput.value
+          const taskDescription = descriptionInput.value
+          const taskStatus = taskSelect.value
+
+          console.log(taskName, taskDescription, taskStatus)
+          createNewTask(boardId, listId, taskName, taskDescription, taskStatus)
+        } else {
+          console.error("Can't get the values from alert inputs, returning")
+          return
+        }
         return
       } else {
         return
@@ -127,7 +169,7 @@ const BoardPage = () => {
     })
   }
 
-  const createNewTask = ({ listId, boardId, taskName }: { listId: string, boardId: string, taskName: string }) => {
+  const createNewTask = (boardId: string, listId: string, taskName: string, taskDescription: string, taskStatus: string) => {
     if (user && !Object.keys(user).length) return
 
     console.log("List id: ", listId)
@@ -153,7 +195,8 @@ const BoardPage = () => {
     const newTask: Task = {
       id: uniqueId,
       name: `${taskName ? taskName : "New task"}`,
-      status: "",
+      description: taskDescription,
+      status: taskStatus,
     }
 
     const updatedBoards = boards
@@ -176,16 +219,50 @@ const BoardPage = () => {
     updateData()
   }
 
+  const handleEditTask = (taskName: string, taskDescription: string | undefined, taskStatus: string) => {
+    Swal.fire({
+      color: "#fff",
+      background: "#111827",
+      title: 'There will be task editing soon',
+      showConfirmButton: true,
+      confirmButtonText: "Create",
+      confirmButtonColor: "#2563eb",
+      showCancelButton: true,
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let newTaskName: string
+
+        if (result.value === taskName || !result.value) {
+          newTaskName = taskName
+          editTask(newTaskName, taskDescription, taskStatus)
+          return
+        } else {
+          newTaskName = result.value
+          editTask(newTaskName, taskDescription, taskStatus)
+        }
+      } else {
+        return
+      }
+    })
+  }
+
+  const editTask = (taskName: string, taskDescription: string | undefined, taskStatus: string) => {
+    return
+  }
+
   return (
     <div className="poppins w-screen flex flex-row flex-wrap gap-2 items-start justify-center p-5 text-center text-white">
       {board && board.lists.map((list) => (
         <div className="flex flex-col items-center justify-center rounded-3xl w-[350px] py-2 px-3 bg-gray-900 text-white gap-1" key={list.id}>
           <h1 className="text-1xl py-1">{list.name}</h1>
           {list.tasks.map((task) => (
-            <div className="flex flex-col rounded-2xl cursor-pointer hover:bg-gray-700 standard_transition bg-gray-800 w-[100%] py-1 text-start ps-4" key={task.id}>
-              <h1 className="text-1xl">{task.name}</h1>
-              <span className="text-1xl">{task.description}</span>
-              <span className="text-1xl">{task.status}</span>
+            <div className="flex flex-col rounded-2xl cursor-pointer hover:bg-gray-700 standard_transition bg-gray-800 w-[100%] py-0 text-start" key={task.id} onClick={() => handleEditTask(task.name, task.description, task.status)}>
+              <div className="flex justify-between items-center">
+                <h1 className="ps-4 py-1 text-1xl w-[calc(100%-40px)] break-words">{task.name}</h1>
+                <span className="w-[24px] h-[24px] mr-1 flex items-center justify-center" dangerouslySetInnerHTML={{ __html: task.status === "unfinished" ? "&#x1F534;" : task.status === "inProgress" ? "&#x1F7E1;" : task.status === "finished" ? "&#x1F7E2;" : "&xcirc;" }}></span>
+              </div>
+              <span className="text-1xl bg-gray-500 ps-4 rounded-t-none rounded-bl-2xl rounded-br-2xl">{task.description}</span>
             </div>
           ))}
           <div className="rounded-full cursor-pointer hover:bg-gray-700 standard_transition bg-gray-800 w-[100%] py-1" onClick={() => handleCreateNewTask({ listId: list.id, boardId: board.id })}>+ Create new task</div>
